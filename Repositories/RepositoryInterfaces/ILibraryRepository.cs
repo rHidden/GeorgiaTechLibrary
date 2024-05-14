a6526e9200e8d914ad176ebf6dbcc42d91f0283b
rHidden <davidbuncek@gmail.com>
2024-05-14 14:40:29 +0200

    remove comments

--- Repositories/RepositoryInterfaces/ILibraryRepository.cs
+++ Repositories/RepositoryInterfaces/ILibraryRepository.cs
@@ -7,7 +7,7 @@ namespace GeorgiaTechLibrary.Repositories.RepositoryInterfaces
         Task<Library> CreateLibrary(Library library);
         Task UpdateLibrary(Library library);
         Task<Library> GetLibrary(string libraryName);
-        Task DeleteLibrary(string libraryName); //Will it be possible to just remove a library?
-        Task<List<Library>> ListLibraries(); //Probably not necessary - maybe only if the user wants to see all libraries?
+        Task DeleteLibrary(string libraryName);
+        Task<List<Library>> ListLibraries();
     }
 }
--- Repositories/RepositoryInterfaces/ILoanRepository.cs
+++ Repositories/RepositoryInterfaces/ILoanRepository.cs
@@ -7,7 +7,7 @@ namespace GeorgiaTechLibrary.Repositories.RepositoryInterfaces
         Task<Loan> CreateLoan(Loan loan);
         Task<Loan> GetLoan(int id);
         Task UpdateLoan(Loan loan);
-        Task DeleteLoan(int id);  //Should we actually allow?
+        Task DeleteLoan(int id);
         Task<List<Loan>> ListUserLoans(string userSSN);
     }
 }
