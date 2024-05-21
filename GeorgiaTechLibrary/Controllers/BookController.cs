b70ca25d8e6d53835e953dd46d6ec59f5f68335c
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2024-05-21 18:11:05 +0200

    Initial Swagger documenation

--- GeorgiaTechLibrary/Controllers/BookController.cs
+++ GeorgiaTechLibrary/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccess.Models;
 using GeorgiaTechLibrary.Services.ServiceInterfaces;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace GeorgiaTechLibrary.Controllers
 {
@@ -14,6 +15,13 @@ namespace GeorgiaTechLibrary.Controllers
             _bookService = bookService;
         }
 
+        /// <summary>
+        /// Retrieves a list of all books.
+        /// </summary>
+        /// <description>
+        /// Description: This endpoint retrieves a list of all books in the database.
+        /// </description>
+        /// <returns>A list of books.</returns>
         [HttpGet]
         [Route("{ISBN}")]
         public async Task<IActionResult> GetBookAsync(string ISBN)
--- GeorgiaTechLibrary/Program.cs
+++ GeorgiaTechLibrary/Program.cs
@@ -4,13 +4,23 @@ using DataAccess.Repositories.RepositoryInterfaces;
 using GeorgiaTechLibrary.Services;
 using GeorgiaTechLibrary.Services.ServiceInterfaces;
 using DataAccess.DAO.DAOIntefaces;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
@@ -25,7 +35,7 @@ builder.Services.AddCors(options =>
 // Dependency Injection
 //services
 builder.Services.AddScoped<IBookService, BookService>();
-builder.Services.AddScoped<ILibraryService, LibraryService>(); 
+builder.Services.AddScoped<ILibraryService, LibraryService>();
 //builder.Services.AddScoped<ILoanService, LoanService>();
 builder.Services.AddScoped<IMemberService, MemberService>();
 builder.Services.AddScoped<IStaffService, StaffService>();
@@ -41,12 +51,16 @@ builder.Services.AddScoped<IDatabaseConnectionFactory, DatabaseConnectionFactory
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseCors("CorsPolicy");
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "GeorgiaTechLibrary");
+});
 
 app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
 
 app.Run();
