058afc50f6b8285460fa0b11c60f6e4fe2b07f5b
AzizKadem <93520332+AzizKadem@users.noreply.github.com>
2024-05-22 18:11:06 +0200

    Prettify queries

--- GeorgiaTechLibrary/Scripts/5 Queries - very optimized/ItemsPercentages.sql
+++ GeorgiaTechLibrary/Scripts/5 Queries - very optimized/ItemsPercentages.sql
@@ -1,6 +1,10 @@
-SELECT CAST(COUNT(l.BookInstanceId) as FLOAT)/COUNT(l.Id)*100 as Books,
-CAST((SELECT COUNT(di.Id) FROM DigitalItem di RIGHT JOIN Loan l on di.Id = l.DigitalItemId WHERE DigitalItemType = 'Video')as FLOAT)/COUNT(l.Id)*100 as Videos,
-CAST((SELECT COUNT(di.Id) FROM DigitalItem di RIGHT JOIN Loan l on di.Id = l.DigitalItemId WHERE DigitalItemType = 'Audio')as FLOAT)/COUNT(l.Id)*100 as Audios,
-CAST((SELECT COUNT(di.Id) FROM DigitalItem di RIGHT JOIN Loan l on di.Id = l.DigitalItemId WHERE DigitalItemType = 'Text')as FLOAT)/COUNT(l.Id)*100 as Texts,
-CAST((SELECT COUNT(di.Id) FROM DigitalItem di RIGHT JOIN Loan l on di.Id = l.DigitalItemId WHERE DigitalItemType = 'Image')as FLOAT)/COUNT(l.Id)*100 as Images
-FROM Loan l
+SELECT 
+    CAST(COUNT(l.BookInstanceId) AS FLOAT) / COUNT(l.Id) * 100 AS Books,
+    CAST(SUM(CASE WHEN di.DigitalItemType = 'Video' THEN 1 ELSE 0 END) AS FLOAT) / COUNT(l.Id) * 100 AS Videos,
+    CAST(SUM(CASE WHEN di.DigitalItemType = 'Audio' THEN 1 ELSE 0 END) AS FLOAT) / COUNT(l.Id) * 100 AS Audios,
+    CAST(SUM(CASE WHEN di.DigitalItemType = 'Text' THEN 1 ELSE 0 END) AS FLOAT) / COUNT(l.Id) * 100 AS Texts,
+    CAST(SUM(CASE WHEN di.DigitalItemType = 'Image' THEN 1 ELSE 0 END) AS FLOAT) / COUNT(l.Id) * 100 AS Images
+FROM 
+    Loan l
+LEFT JOIN 
+    DigitalItem di ON di.Id = l.DigitalItemId;
