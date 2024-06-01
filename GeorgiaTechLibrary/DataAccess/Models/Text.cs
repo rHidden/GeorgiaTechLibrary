c19ddfa701b5e46a3c08c4364ca61412826fe7c8
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2024-06-01 12:23:50 +0200

    Update Text.cs

--- GeorgiaTechLibrary/DataAccess/Models/Text.cs
+++ GeorgiaTechLibrary/DataAccess/Models/Text.cs
@@ -4,7 +4,7 @@
     {
         public Text() { }
 
-        public Text(string name, List<string> authors, int id, string format, 
+        public Text(string name, List<string> authors, int id, string format,
             double size) : base(name, authors, id, format, size) { }
         public Text(DigitalItem digitalItem) : base(digitalItem) { }
     }
